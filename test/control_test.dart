007412fdeee11ea17be9249bdf34fe1d75ca0ba6
Plague Fox <plugfox@gmail.com>
2023-12-28 21:12:34 +0400

    Update launch.json

--- test/control_test.dart
+++ test/control_test.dart
@@ -1,7 +1,16 @@
+// ignore_for_file: unnecessary_lambdas
+
 import 'package:flutter_test/flutter_test.dart';
 
+import 'widget/state_scope_test.dart' as state_scope_test;
+
 void main() {
-  test('placeholder', () {
-    expect(true, isTrue);
+  group('unit', () {
+    test('placeholder', () {
+      expect(true, isTrue);
+    });
+  });
+  group('widget', () {
+    state_scope_test.main();
   });
 }
